e31548ff3d799af8f7f23350be02c19a79551bec
Sheefoo <n.almalaika@yahoo.com>
2024-08-06 21:12:29 +0200

    apply the REST API functionality

--- src/main/java/Question/QuestionRepository.java
+++ src/main/java/Question/QuestionRepository.java
@@ -1,8 +1,15 @@
 package Question;
 
+import java.io.Serializable;
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
 
-public interface QuestionRepository extends CrudRepository<Question, Long> {
-    List<Question> findAll();
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Serializable> {
+
+    //@Query("SELECT q FROM Question ")
+    // public List<Question> findAll();
 }
