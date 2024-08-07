28e0d3828b2adf573558ba6f7899da58374f9dbe
Sheefoo <n.almalaika@yahoo.com>
2024-08-08 00:21:34 +0200

    fetch data from json and save to db table (question_model)

--- src/main/java/Nazek/Quiz/QuizApplication.java
+++ src/main/java/Nazek/Quiz/QuizApplication.java
@@ -1,47 +1,21 @@
 package Nazek.Quiz;
 
-import Question.Question;
-import Question.QuestionRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import java.io.PrintStream;
-import java.util.List;
-
-import Question.QuestionController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication(scanBasePackages = "Nazek.Quiz")
 @EnableJpaRepositories(basePackages = "Question")
 @EntityScan(basePackages = "Question")
 @ComponentScan(basePackages = "Question")
+@ComponentScan(basePackages = "Nazek.Quiz")
 public class QuizApplication {
-	// private QuestionRepository questionRepository;
 
-	/*public QuizApplication() {
-	}*/
-	public static void main(String[] args) {
-		SpringApplication.run(QuizApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(QuizApplication.class, args);}
+
 	}
-		/*EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		List<Question> questions = entityManager.createQuery("SELECT q FROM Question q").getResultList();
-		PrintStream printstr = System.out;
-		if (questions == null) {
-			System.out.println("No questions found . ");
-		} else {
-			for (Question ques : questions){
-				printstr.println("Question= " + ques.getQuestion() + ", Question id " + ques.getId());
-			}
-
-		}
-
-		entityManager.close();
-		entityManagerFactory.close();*/
-
-}
--- src/main/java/Question/QuestionRepository.java
+++ src/main/java/Question/QuestionRepository.java
@@ -1,15 +1,17 @@
 package Question;
 
 import java.io.Serializable;
-import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+/*@Repository
 public interface QuestionRepository extends JpaRepository<Question, Serializable> {
 
-    //@Query("SELECT q FROM Question ")
-    // public List<Question> findAll();
+}*/
+
+@Repository
+public interface QuestionRepository extends ListCrudRepository<QuestionModel, Serializable> {
+
 }
+
