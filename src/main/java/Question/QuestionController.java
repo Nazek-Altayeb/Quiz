98e697d1475eabda28dee1315d778e63a28db5d0
Sheefoo <n.almalaika@yahoo.com>
2024-08-08 09:53:44 +0200

    remove unused file

--- src/main/java/Question/QuestionController.java
+++ src/main/java/Question/QuestionController.java
@@ -31,20 +31,4 @@ public class QuestionController {
             return new ResponseEntity<>(allQuestions, HttpStatus.OK);
     }
 
-
-
-    /*@GetMapping(path= "/allQuestions")
-    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-    public List<String> getAllQuestions(){
-        RestTemplate restTemplete = new RestTemplate();
-        ResponseEntity<QuestionModel[]> data = restTemplete.getForEntity("Data/OpenTRIVIA_Response.Json", QuestionModel[].class);
-        QuestionModel[] objects = data.getBody();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        return Arrays.stream(objects)
-                .map(object -> mapper.convertValue(object, Question.class))
-                .map(Question::getQuestion)
-                .collect(Collectors.toList());
-    }*/
-
 }
